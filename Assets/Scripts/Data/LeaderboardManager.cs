33cd222423f6e914d1fccb0ef03fe2e587784dc6
NeavPanjwani56 <Neav.Panjwani.5062582@ves.ac.in>
2026-03-27 00:55:21 +0530

    data storing system updated

--- Assets/Scripts/Data/LeaderboardManager.cs
+++ Assets/Scripts/Data/LeaderboardManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LeaderboardManager : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class LeaderboardManager : MonoBehaviour
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        LoadData();
     }
 
     public void AddRun(RunData data)
@@ -23,10 +25,42 @@ public class LeaderboardManager : MonoBehaviour
 
         // sort by score descending
         runs.Sort((a, b) => b.score.CompareTo(a.score));
+        SaveData();
     }
 
     public List<RunData> GetTopRuns(int count)
     {
         return runs.GetRange(0, Mathf.Min(count, runs.Count));
+    }
+
+    // save data function 
+
+    void SaveData()
+    {
+        LeaderboardData data = new LeaderboardData();
+        data.runs = runs;
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.persistentDataPath + "/leaderboard.json", json);
+
+        Debug.Log("DATA SAVED: " + Application.persistentDataPath);
+    }
+
+    void LoadData()
+    {
+        string path = Application.persistentDataPath + "/leaderboard.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            LeaderboardData data = JsonUtility.FromJson<LeaderboardData>(json);
+            runs = data.runs;
+
+            Debug.Log("DATA LOADED");
+        }
+        else
+        {
+            Debug.Log("NO SAVE FILE FOUND");
+        }
     }
 }
---
+++ Assets/Scripts/datastore/LeaderboardData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class LeaderboardData
+{
+    public List<RunData> runs = new List<RunData>();
+}
